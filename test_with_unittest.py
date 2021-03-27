1271b2318c6599f50dc7e6151d13df11836f8b9c
Nayan Biswas <nayan32biswas@gmail.com>
2021-03-27 13:06:48 +0600

    test passed

--- test_with_unittest.py
+++ test_with_unittest.py
@@ -12,11 +12,14 @@ class TestProblemOne(unittest.TestCase):
                 "key4": {
                     "key5": 4
                 },
-                "key5": {
-                }
+                "key6": {},
+                "key7": [1, 2, 3],
+                "key8": {
+                    "key9": "string",
+                    "key 10": 0
+                },
             }
         }
-        
         return super().setUp()
 
     def match_all_keys(self, x, y):
@@ -29,13 +32,13 @@ class TestProblemOne(unittest.TestCase):
         return True
 
     def test_output(self):
-        results = ["key1 1","key2 1","key3 2","key4 2","key5 3",]
+        results = ["key1 1","key2 1","key3 2","key4 2","key5 3", "key6 2","key7 2","key8 2","key9 3","key 10 3",]
         obj = SolutionOne(self.data)
         executed_results = obj.get_results()
         self.assertEqual(results, executed_results)
 
     def test_all_key(self):
-        results = ["key1","key2","key3","key4","key5"]
+        results = ["key1","key2","key3","key4","key5", "key6","key7","key8","key9","key 10"]
         obj = SolutionOne(self.data)
         executed_results = obj.get_results()
         self.assertTrue(self.match_all_keys(results, executed_results))
