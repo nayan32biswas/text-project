007c565ba92af4c0173ca8ecc83006df846af6f8
Nayan Biswas <nayan32biswas@gmail.com>
2021-03-27 13:00:21 +0600

    updata by BOT

--- solutions.py
+++ solutions.py
@@ -1,7 +1,42 @@
-print("One")
-
-
 class SolutionOne:
+    data = {
+        "key1": 1,
+        "key2": {
+            "key3": 1,
+            "key4": {
+                "key5": 4
+            }
+        }
+    }
+    
+
+    def __init__(self, data=None) -> None:
+        if data:
+            self.data = data
+        pass
+
+    def depth_in_dict(self, obj, depth) -> None:
+        if type(obj) != dict:
+            return
+        for key, value in obj.items():
+            self.results.append(f"{key} {depth}")
+            if type(value) == dict:
+                self.depth_in_dict(value, depth+1)
+
+    def get_results(self):
+        self.results = []
+        self.depth_in_dict(self.data, 1)
+        return self.results 
+
+    def start(self):
+         self.get_results()
+         for value in self.results:
+             print(value)
+
+
+
+
+class SolutionTwo:
     data = {
         "key1": 1,
         "”key2": {
@@ -11,17 +46,46 @@ class SolutionOne:
             }
         }
     }
+    results = []
+
     def __init__(self, data=None) -> None:
         if data:
             self.data = data
         pass
-    def results(self):
+
+    def get_results(self):
         return []
 
-class SolutionTwo:
-    def results(self):
-        return []
+    def start(self):
+        self.results = []
+
 
 class SolutionThree:
     def results(self):
         return []
+    def start(self):
+        pass
+
+
+def main():
+    runing = 1
+    while(runing):
+        user_input = input("Type 1 or 2 or 3 to execute solution and 0 to exit: ")
+        if '1' in user_input:
+            SolutionOne().start()
+
+        elif '2' in user_input:
+            SolutionTwo().start()
+        elif '3' in user_input:
+            SolutionThree().start()
+        elif '0' in user_input:
+            return 0
+        else:
+            runing = 0
+            user_input = None
+            SolutionOne().start()
+            SolutionTwo().start()
+            SolutionThree().start()
+
+if __name__ == "__main__":
+    main()
--- test_with_unittest.py
+++ test_with_unittest.py
@@ -5,7 +5,7 @@ from solutions import SolutionOne
 
 class TestProblemOne(unittest.TestCase):
     def setUp(self) -> None:
-        data = {
+        self.data = {
             "key1": 1,
             "key2": {
                 "key3": 1,
@@ -21,7 +21,7 @@ class TestProblemOne(unittest.TestCase):
         if len(x) != len(y):
             return False
         for value in y:
-            value = " ".join(value.split[:-1])
+            value = " ".join(value.split()[:-1])
             if value not in x:
                 return False
         return True
@@ -29,11 +29,11 @@ class TestProblemOne(unittest.TestCase):
     def test_output(self):
         results = ["key1 1","key2 1","key3 2","key4 2","key5 3",]
         obj = SolutionOne(self.data)
-        executed_results = obj.results()
+        executed_results = obj.get_results()
         self.assertEqual(results, executed_results)
 
     def test_all_key(self):
         results = ["key1","key2","key3","key4","key5"]
         obj = SolutionOne(self.data)
-        executed_results = obj.results()
+        executed_results = obj.get_results()
         self.assertTrue(self.match_all_keys(results, executed_results))
